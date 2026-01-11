625cbb3fe11f4f46baff0d50af2bedf3f3f86a22
uditsaha28-alt <uditsaha28@gmail.com>
2026-01-11 06:11:13 +0000

    conversion of datatypes and typeof func use

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -1,8 +1,12 @@
 console.log("Udit saha")
 let age
-console.log(age)
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof 26)
-console.log(typeof "Udit")
+// console.log(age)
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof 26)
+// console.log(typeof "Udit")
+let isLoggedIn="xxx"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
 
