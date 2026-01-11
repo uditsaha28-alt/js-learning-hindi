afba300dfd3ec6d86f7b85872921ac4ccde85f03
uditsaha28-alt <uditsaha28@gmail.com>
2026-01-11 14:06:18 +0000

    operations nd string and number

--- .devcontainer/01_basics/operations.js
+++ .devcontainer/01_basics/operations.js
@@ -1,3 +1,12 @@
 let value=3
 let negvalue=-value
-console.log(negvalue)
+console.log(negvalue)  
+console.log("1" +2+"3")
+console.log(+true)
+console.log(+"")
+let gc=100
+gc--
+console.log(gc)
+
+
+ 
