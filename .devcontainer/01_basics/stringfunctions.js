33b3be8227c6eb7e352cbb2512f26aeefc28647e
uditsaha28-alt <uditsaha28@gmail.com>
2026-01-13 14:34:57 +0000

    string func

---
+++ .devcontainer/01_basics/stringfunctions.js
@@ -0,0 +1,13 @@
+const names=new String("UditSaha")
+console.log(names)
+console.log(names.__proto__)
+console.log(names.length);
+console.log(names.toLowerCase())
+console.log(names[2])
+const name="Udit Saha"
+const age=18
+console.log('Hello my name is ${name} and my age is ${age}')
+const sur=new String("Saha-Das_Pal")
+console.log(sur.split('-'))
+console.log(sur.replace('-','.'))
+console.log(sur.includes('pal'))
