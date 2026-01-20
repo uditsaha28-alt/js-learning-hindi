81d153d3116765619f2eb24f2df3d05c869da7ac
uditsaha28-alt <uditsaha28@gmail.com>
2026-01-20 09:55:49 +0000

    scope of variables and functions

---
+++ .devcontainer/01_basics/scope.js
@@ -0,0 +1,22 @@
+// let a=100
+// if(true){
+//     let a=10
+//     let b=20
+//     const c=30
+// }
+// console.log(a);
+
+// var a=100
+// if(true){
+//     var a=10
+//     let b=20
+//     const c=30
+//     console.log("INNER",a)
+// }
+// console.log("OUTER",a);
+
+function add(num){
+    return num+5
+}
+const ans=add(2)
+console.log(ans)
