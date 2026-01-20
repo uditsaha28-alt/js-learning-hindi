6a7f5e7451a7560549efa498ed4a66767342aab8
uditsaha28-alt <uditsaha28@gmail.com>
2026-01-20 14:26:23 +0000

    loops

---
+++ .devcontainer/01_basics/loops.js
@@ -0,0 +1,11 @@
+// const array=[1,2,3,4,5,6,7,8,9,10];
+// console.log(array.length);
+// for(let index=1;index<array.length;index++){
+//     if(index%2==0){
+//         console.log(index);
+//     }
+//}
+
+//backticks
+const user="Udit Saha";
+console.log(`Welcome back, ${user}.I hope u are having a good time ${user}`);
